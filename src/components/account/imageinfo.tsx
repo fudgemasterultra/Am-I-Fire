86798001b6b3c53d087590401572d500a58c990b
fudgemasterultra <videogamecreative@gmail.com>
2024-02-11 17:44:51 -0500

    added in account page

---
+++ src/components/account/imageinfo.tsx
@@ -0,0 +1,50 @@
+import { query, collection, where } from "firebase/firestore";
+import { useCollection } from "react-firebase-hooks/firestore";
+import { db } from "../../../firebase/firebase";
+import { Types } from "@/types/types";
+import { Card, CardContent, CardMedia } from "@mui/material";
+import { ThumbDownSharp, ThumbUpSharp } from "@mui/icons-material";
+
+export default function AccountImageView({ uid }: { uid: string }) {
+  const imageRef = collection(db, "images");
+  const q = query(imageRef, where("owner", "==", uid));
+  const [value, loading, error] = useCollection(q);
+
+  return (
+    <>
+      <div className="flex flex-wrap gap-3 justify-center">
+        {value
+          ? value.docs.map((doc) => {
+              const imageInfo = doc.data() as Types.ImageDoc;
+              const likes = imageInfo.likes.length;
+              const dislike = imageInfo.dislikes.length;
+              const total = likes + dislike;
+              return (
+                <div key={doc.id}>
+                  <Card sx={{ width: 300 }}>
+                    <CardMedia
+                      sx={{ height: 533, objectFit: "cover" }}
+                      image={imageInfo.url}
+                      title="Image"
+                    />
+                    <CardContent>
+                      <div className="flex content-center justify-between">
+                        <div className="flex">
+                          <ThumbUpSharp />
+                          <p>{Math.round((likes / total) * 100) || 0}%</p>
+                        </div>
+                        <div className="flex">
+                          <ThumbDownSharp />
+                          <p>{Math.round(dislike / total) * 100 || 0}%</p>
+                        </div>
+                      </div>
+                    </CardContent>
+                  </Card>
+                </div>
+              );
+            })
+          : null}
+      </div>
+    </>
+  );
+}
--- src/pages/account/index.tsx
+++ src/pages/account/index.tsx
@@ -2,6 +2,7 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../../../firebase/firebase";
 import { useRouter } from "next/router";
 import UploadButton from "@/components/account/upload";
+import AccountImageView from "@/components/account/imageinfo";
 
 export default function Index() {
   const [user, loading, error] = useAuthState(auth);
@@ -15,6 +16,7 @@ export default function Index() {
   if (user) {
     return (
       <>
+        <AccountImageView uid={user.uid} />
         <UploadButton uid={user.uid} />
       </>
     );
